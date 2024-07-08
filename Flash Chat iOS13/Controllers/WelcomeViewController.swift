d692edeed9a2cdcb574bd5470c6fd4dd7c328c4d
Zhanat <62037412+ZhanatShengelbay@users.noreply.github.com>
2024-07-08 22:06:29 +0200

    navigation, tableView and firebase usage

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -1,10 +1,4 @@
-//
-//  WelcomeViewController.swift
-//  Flash Chat iOS13
-//
-//  Created by Angela Yu on 21/10/2019.
-//  Copyright © 2019 Angela Yu. All rights reserved.
-//
+
 
 import UIKit
 
@@ -12,8 +6,23 @@ class WelcomeViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
     
+    @IBAction func registerPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func loginPressed(_ sender: UIButton) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        titleLabel.text = ""
+        var charIndex = 0.0
+        let titleText = K.appName
+        for letter in titleText {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) {
+                (timer) in self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
 
        
     }
